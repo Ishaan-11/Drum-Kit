1438e1ec0c86f8a8346cc602402c6bbce730aa7d
Ishaan-11 <ishaansharma1998@gmail.com>
2020-07-25 21:35:40 +0530

    12.6

--- index.js
+++ index.js
@@ -1,20 +1,9 @@
-// document.querySelector("button").addEventListener("click",handleClick);
-
-/* function handleClick() {
-    alert("I got clicked");
-} */
-
-//anonymous function
-/* document.querySelector("button").addEventListener("click", function () {
-    alert("i got clicked");
-
-    //what to do when click detected.
-}); */
-
 var drumButtons = document.querySelectorAll(".drum");
 
 for(var i=0; i<drumButtons.length; i++) {
     drumButtons[i].addEventListener("click", function () {
-        alert("I got clicked!");
+        /* var audio = new Audio('sounds/tom-1.mp3');
+        audio.play(); */
+        this.style.color  = "white";
     });
 }
